a1eefeeadaa771d2d0401571fd0df18e5540da44
JoSantana0005 <joseasantana05@gmail.com>
2024-08-02 18:41:14 -0400

    Se agrega los getters y setters de la clase Pokemon

--- ProyectosenPy/Pokemon.py
+++ ProyectosenPy/Pokemon.py
@@ -1,3 +1,29 @@
 #Batalla de pokemones
 class Pokemon:
+    def __init__(self,nombre,tipo,HP,atq):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.vida = HP
+        self.ataque = atq
     
+    #Getters
+    def getNombre(self):
+        return self.nombre
+    def getTipo(self):
+        return self.tipo
+    def getHP(self):
+        return self.vida
+    def getAtaque(self):
+        return self.ataque
+    
+    #Setters
+    def setNombre(self,nombre):
+        self.nombre = nombre
+    def setTipo(self,tipo):
+        self.tipo = tipo
+    def setHP(self,HP):
+        self.vida = HP
+    def setAtaque(self,Ataque):
+        self.ataque = Ataque
+    
+        
