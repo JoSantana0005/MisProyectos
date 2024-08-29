ac9f113934c06ff009be0b20adea596b9818c527
JoSantana0005 <joseasantana05@gmail.com>
2024-08-28 21:22:54 -0400

    ejercicio mas elaborado

--- ProyectosenPy/Persona.py
+++ ProyectosenPy/Persona.py
@@ -1,43 +1,97 @@
+import math
+import random
 class Persona:
-    #Constructor
-    def __init__(self,nom= str,ap = str,correo = str):
-        self.nom = nom
-        self.ap = ap
-        self.correo = correo
+    def __init__(self, nombre: str, edad: int, dni:str, sexo: str, peso: float, altura: float):
+        self.nombre = nombre
+        self.edad = edad
+        self.dni = dni
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
     
     #Getters
-    def getNom(self):
-        return self.nom
+    def getNombre(self):
+        return self.nombre
     
-    def getApe(self):
-        return self.ap
+    def getEdad(self):
+        return self.edad
     
-    def getCorreo(self):
-        return self.correo
+    def getDni(self):
+        return self.dni
+    
+    def getSexo(self):
+        return self.sexo
+    
+    def getPeso(self):
+        return self.peso
+    
+    def getAltura(self):
+        return self.altura
     
     #Setters
-    def setNom(self,nom):
-        self.nom = nom
+    def setNombre(self,nombre):
+        self.nombre = nombre
     
-    def setApe(self,ap):
-        self.ap = ap
+    def setEdad(self,edad):
+        self.edad = edad
     
-    def setCorreo(self,correo):
-        self.correo = correo
-
+    def setDni(self,dni):
+        self.dni = dni
+    
+    def setSexo(self,sexo):
+        self.sexo = sexo
+    
+    def setPeso(self,peso):
+        self.peso = peso
+    
+    def setAltura(self,altura):
+        self.altura = altura
+    
     #Funciones
-    def Presentarse(self):
-        print("Mi nombre es: " + self.nom)
-        print("Mi apellido es: " + self.ap)
-        print("me puedes contactar por aqui: " + self.correo)
 
-# Principal
-nom = input("Ingrese el nombre de la persona: ")
-apelli = input("Ingrese el apellido de la persona: ")
-corre = input("Ingrese el correo de la persona: ")
-
-perso1 = Persona(nom,apelli,corre)
-person2 = Persona("Jose","Santana","joseasantana05@gmail.com")
-perso1.Presentarse()
-print("------------------------")
-person2.Presentarse()
+    #Calculo del peso de la persona
+    def ClaculoMC(self):
+        ideal = 0
+        pesoActual = self.peso/math.pow(self.altura,2)
+        print(pesoActual)
+        if(pesoActual < 20):
+            ideal = -1
+            return ideal
+        elif(pesoActual >= 20 and pesoActual <= 25):
+            return ideal
+        else:
+            ideal = 1
+            return ideal
+    #Comparar sexo
+    def comparar__sexo(self):
+        if(self.sexo.lower() == "h" or self.sexo.lower() == "m"):
+            print("Introdujo los datos correctos para el sexo")
+        else:
+            raise ValueError("Introdujo los datos incorrectos de sexo")
+    
+    def mostrar__datos(self):
+        print("Datos de la persona")
+        print("Nombre      edad      dni       sexo (h/m)     peso      altura")
+        print("{0:^10s} {1:^10d} {2:^10s} {3:^10s} {4:^10.2f} {5:^10.2f}".format(self.nombre,self.edad,
+                                                                                 self.dni,self.sexo,self.peso,self.altura))
+    
+def main():
+    try:
+        nombre = input("Ingrese el nombre de la persona: ")
+        edad = int(input("Ingrese la edad de la persona: "))
+        dni = str(random.randint(10000000,99999999))
+        sexo = input("Ingrese el sexo de la persona: ")
+        peso = float(input("Ingrese el peso de la persona: "))
+        altura = float(input("Ingrese la altura de la persona: "))
+        persona = Persona(nombre,edad,dni,sexo,peso,altura)
+        result = persona.ClaculoMC()
+        if(result == -1):
+            print("Esta en su peso ideal")
+        elif(result == 0):
+            print("Esta por debajo de su peso ideal")
+        else:
+            print("Tiene sobrepeso")
+        persona.mostrar__datos()
+    except:
+        raise ValueError("Error: error de dato")
+system = main()
