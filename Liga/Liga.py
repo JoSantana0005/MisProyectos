7c2425d26a1dbc01a56d00d6e56307c60e5bb239
JoSantana0005 <joseasantana05@gmail.com>
2024-09-17 13:31:19 -0400

    The functions of adding the league and modifying the league begin to be added

--- Liga/Liga.py
+++ Liga/Liga.py
@@ -3,7 +3,7 @@ class Liga:
         self.id = id
         self.nombre = nombreLiga
         self.Pais = PaisLiga
-        self.equipos = []
+        self.Ligas = {}
     
     #Getters 
     def getNombreLiga(self):
@@ -18,5 +18,28 @@ class Liga:
     
     def setPaisLiga(self,Pais):
         self.Pais = Pais
+    
+
+    #Function
+    #Function that adds the name of the league and the country in a dictionary
+    def add__data(self):
+        self.Ligas['ID'] = self.id
+        self.Ligas['Nombre de la liga'] = self.nombre
+        self.Ligas['Pais de la liga'] = self.Pais
+        return self.Ligas
+    #Function Function that modifies the dictionary data
+    def edit__data(self, Ligas: list, nombre: str, pais: str):
+        try:
+            id = int(input("Ingrese el id de la liga que quiere modificar: "))
+            for Liga in Ligas:
+                if Liga['ID'] == id:
+                    Liga.update({
+                        'Nombre de la liga': nombre,
+                        'Pais de la liga': pais
+                    })
+                else:
+                    print("The link to modify was not found")
+        except:
+            raise ValueError("Error: data invalid")
     
     
