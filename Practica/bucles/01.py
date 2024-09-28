e90542ea559b661d69df24b39d4d40353d959d75
JoSantana0005 <joseasantana05@gmail.com>
2024-09-28 13:06:53 -0400

    .

---
+++ Practica/bucles/01.py
@@ -0,0 +1,6 @@
+"""Escribi un programa que pida al usuario una palabra y la muestre por pantalla 10 veces"""
+
+#Palabra
+palabra = input("Ingrese la palabra que quiere que se imprima: ")
+for i in range(0,10):
+    print(palabra)
